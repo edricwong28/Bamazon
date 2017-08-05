a059774790e5b822419a7628de051f80ae702299
edricwong28 <edricwong28@gmail.com>
2017-08-04 19:10:08 -0700

    Bamazon

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,73 +2,111 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 require("console.table");
 
-// Initializes the connection variable to sync with a MySQL database
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "English2h!",
   database: "bamazon"
 });
 
-//Connecting to the mySQL database, which is listening on port 3306
 connection.connect(function(err) {
-	if(err){	
-  	console.log(err);
-	};
-    console.log("connection successful!")
-    showProducts();
+  if (err) {
+    console.error("error connecting: " + err.stack);
+  }else{
+  showInventory();
+  }
 });
 
-
-function showProducts() {
-      console.log("Selecting all products...\n");
-      connection.query("SELECT * FROM products", function(err, res) {
-        if (err) throw err;
-        // Log all results of the SELECT statement
-        console.table(res);
-        askQuestion();
-        connection.end();
-      });
-    }    
-
-function askForId(inventory){
-
-    inquirer.prompt([
-      {
-        name: "id",
-        type: "input",
-        message: "What is the ID of the product you wish to purchase?"
-      }
-    ]).then(function(val) {
-
-    });
-  }
-
-function askForQuantity(){
-  inquirer.prompt([ 
-    {
-        name: "quantity",
-        type: "input",
-        message: "Please enter the quantity of the item you wish to purchase."
+function showInventory() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err){
+      throw err;
     }
-  ]).then(function(quantity){
-
-  })
+    else{
+    console.table(res);
+    askCustomerforItem(res);
+     }
+  });
 }
 
-function makePurchase(){
 
+function askCustomerforItem(inventory) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "choice",
+        message: "What is the ID of the item you would you like to purchase? [Press Q to Quit]",
+        validate: function(val) {
+          return !isNaN(val) || val.toLowerCase() === "q";
+        }
+      }
+    ])
+    .then(function(val) {
+      quit(val.choice);
+      var choiceId = parseInt(val.choice);
+      var product = checkInventory(choiceId, inventory);
+
+      if (product) {
+        askQuantity(product);
+      }
+      else {
+        console.log("\nThat item is not in the inventory.");
+        showInventory();
+      }
+    });
 }
 
-function checkInventory(){
+function askQuantity(product) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "quantity",
+        message: "How many would you like? [Press Q to Quit]",
+        validate: function(val) {
+          return val > 0 || val.toLowerCase() === "q";
+        }
+      }
+    ])
+    .then(function(val) {
+      quit(val.quantity);
+      var quantity = parseInt(val.quantity);
 
+      if (quantity > product.stock_quantity) {
+        console.log("\nInsufficient quantity!");
+        showInventory();
+      }
+      else {
+        makePurchase(product, quantity);
+      }
+    });
 }
 
-function userExit(){
-  
+function makePurchase(product, quantity) {
+  connection.query(
+    "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
+    [quantity, product.item_id],
+    function(err, res) {
+      console.log("\nSuccessfully purchased " + quantity + " " + product.product_name + "'s!");
+      showInventory();
+    }
+  );
+}
+
+function checkInventory(choiceId, res) {
+  for (var i = 0; i < res.length; i++) {
+    if (res[i].item_id === choiceId) {
+      return res[i];
+    }
+  }
+  return null;
+}
+
+function quit(choice) {
+  if (choice.toLowerCase() === "q") {
+    console.log("Goodbye!");
+    process.exit(0);
+  }
 }
